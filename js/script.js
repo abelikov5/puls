2ed5d878388cbc09d63f080b0f8f1961bdfa230b
abelikov5 <abelikov@inbox.ru>
2020-02-12 16:26:55 +0300

    slader && media done, start with catalog

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    $('.slider_wrap').slick();
+    $('.slider_wrap').slick({
+        autoplay: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/4_watch/left.png"> </button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/4_watch/right.png"></button>', 
+        responsive: [
+            {
+              speed: 1200,
+              breakpoint: 968,
+              settings: {
+                arrows: false
+              }
+            }
+          ]
+
+    });
 });
