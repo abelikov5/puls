5bc203543408a6bfee63f8a1af48d5880fdc6e0f
abelikov5 <abelikov@inbox.ru>
2020-02-13 18:12:09 +0300

    92% done, needs to do: module wins, tab for catalog, some UX interfaces, email sending

--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ $(document).ready(function(){
         e.preventDefault();
         $('.cards_wrap_top').eq(i).toggleClass('cards_active_top');
         $('.cards_wrap_back').eq(i).toggleClass('cards_active_back');
+
       });
     });
     $('.cards_back').each(function(i) {
@@ -29,21 +30,44 @@ $(document).ready(function(){
       });
     });
 
-    $('.catalog_list_fitness').each(function(i) {
-      $(this).on('click', function(e) {
-        e.preventDefault();
+// Section CATALOG, scrips for TAB
+    //fitness tab
+    $('.catalog_list_fitness').click(function() {
+      if($('.catalog_list_fitness').hasClass('active_tab')) {}
+      else {
         $('.catalog_list_fitness').toggleClass('active_tab');
-        $('.catalog_list_run').toggleClass('active_tab');
-        $('.catalog_list_triatlon').toggleClass('active_tab');
-      });
+        if($('.catalog_list_run').hasClass('active_tab')) {
+          $('.catalog_list_run').toggleClass('active_tab'); 
+        }
+        else {
+          $('.catalog_list_triatlon').toggleClass('active_tab'); 
+        }
+      }
     });
-    // $('.cards_back').each(function(i) {
-    //   $(this).on('click', function(e) {
-    //     e.preventDefault();
-    //     $('.cards_wrap_top').eq(i).toggleClass('cards_active_top');
-    //     $('.cards_wrap_back').eq(i).toggleClass('cards_active_back');
-    //   });
-    // });
-
-
+    //run tab
+    $('.catalog_list_run').click(function() {
+      if($('.catalog_list_run').hasClass('active_tab')) {}
+      else {
+        $('.catalog_list_run').toggleClass('active_tab');
+        if($('.catalog_list_fitness').hasClass('active_tab')) {
+          $('.catalog_list_fitness').toggleClass('active_tab'); 
+        }
+        else {
+          $('.catalog_list_triatlon').toggleClass('active_tab'); 
+        }
+      }
+    });
+    //triatlon tab
+    $('.catalog_list_triatlon').click(function() {
+      if($('.catalog_list_triatlon').hasClass('active_tab')) {}
+      else {
+        $('.catalog_list_triatlon').toggleClass('active_tab');
+        if($('.catalog_list_run').hasClass('active_tab')) {
+          $('.catalog_list_run').toggleClass('active_tab'); 
+        }
+        else {
+          $('.catalog_list_fitness').toggleClass('active_tab'); 
+        }
+      }
+    });
 });
